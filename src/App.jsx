d240b061b6e12a8d9f378abd8aef5be886fe3ae6
Oliver Ordes <Oliver.ordes17@gmail.com>
2026-06-08 01:54:46 -0600

    created and prepped imports, created and prepped routes to direct users

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { Route, Routes } from "react-router";
+import CatalogPage from "./books/CatalogPage.jsx";
+import Layout from "./layout/Layout.jsx";
+import Error404 from "./Error404";
+import BookDetails from "./books/BookDetails.jsx";
+import Register from "./auth/Register.jsx";
+import Login from "./auth/Login.jsx";
+import Account from "./auth/Account.jsx";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<CatalogPage />} />
+          <Route path="/books" element={<CatalogPage />} />
+          <Route path="/books/:id" element={<BookDetails />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error404 />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
